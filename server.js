804a3bcb41f42a075dc82991dddcfc9f393c6540
Masum <masumc626@gmail.com>
2023-06-11 13:28:16 +0530

    adderd server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 8001;
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+//DB CONNECTION
+const connection = require("./db/connection");
+connection();
+
+//ALL ROUTERS
+const propertyRouter = require("./routes/property");
+const userRouter = require("./routes/user");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", propertyRouter);
+app.use("/", userRouter);
+app.listen(PORT || process.env.PORT, () => { console.log(`server started at PORT ${PORT}`) })
